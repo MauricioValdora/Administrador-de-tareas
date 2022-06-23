70bc8bad6ee54ead58ae779f14938eb550a9e3bc
mauriciovaldora <mauricio_valdora@hotmail.com>
2022-06-23 12:57:30 -0300

    Empesando la aplicacion

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+     <div className='logo-contenedor'>
+      <img 
+        src='{}'
+        className='logo' 
+        />
+     </div>
+    </div>
+  );
+}
+
+export default App;
