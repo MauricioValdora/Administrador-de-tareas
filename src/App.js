a25e2d05f340aa83515be6af7928e6b75de150a5
mauriciovaldora <mauricio_valdora@hotmail.com>
2022-06-23 14:05:56 -0300

    sume estilos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import logo3 from './img/logo3.jpg';
+import Tarea from './componentes/tarea'
 
 function App() {
   return (
     <div className="aplicacion-tareas">
      <div className='logo-contenedor'>
       <img 
-        src='{}'
+        alt='img'
+        src={logo3}
         className='logo' 
         />
      </div>
+     <div className='tareas-lista-principal'>
+      <h1>Mis Tareas</h1> 
+      <Tarea/>
+      </div>
     </div>
   );
 }
